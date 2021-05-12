a7aeb12dfb056dbdf39ed47a20fc6bee5d71a0bf
Ville Jussila <71697703+villejussila@users.noreply.github.com>
2021-05-12 13:07:58 +0300

    fix progress bar not tracking progress

--- src/components/Home/CompletedPomodoros/index.tsx
+++ src/components/Home/CompletedPomodoros/index.tsx
@@ -1,7 +1,7 @@
 import "./CompletedPomodoros.css";
 import ProgressLine from "./ProgressLine";
 import { useAppSelector, useAppDispatch } from "../../App/hooks";
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import {
   cycleCompleted,
   pomodoroCount,
@@ -31,19 +31,25 @@ const CompletedPomodoros = () => {
     [pomodoro.cycleCompleted, dispatch]
   );
 
+  //Updating progress only once a second for better performance
+  const latestRef = useRef<string>();
   useEffect(() => {
     if (timer.timerMode !== "WORK") return;
-    let time = timer.time.timeMs;
-    const pomodoroCountMultiplier = 4;
-    const progressCalculated = 100 - time * pomodoroCountMultiplier || 0;
-    const currentProgressIndex = timer.timerCurrentModeIndex / 2;
-    dispatch(pomodoroCount(currentProgressIndex));
-    if (!timer.isStopped) {
-      dispatch(
-        updateProgresses(
-          getUpdatedProgressArray(progressCalculated, currentProgressIndex)
-        )
-      );
+    if (!latestRef.current || latestRef.current !== timer.time.timeStr) {
+      let latest = timer.time.timeStr;
+      let time = timer.time.timeMs / 60000;
+      const pomodoroCountMultiplier = 4;
+      const progressCalculated = 100 - time * pomodoroCountMultiplier || 0;
+      const currentProgressIndex = timer.timerCurrentModeIndex / 2;
+      dispatch(pomodoroCount(currentProgressIndex));
+      if (!timer.isStopped) {
+        dispatch(
+          updateProgresses(
+            getUpdatedProgressArray(progressCalculated, currentProgressIndex)
+          )
+        );
+      }
+      latestRef.current = latest;
     }
   }, [
     timer.time,
